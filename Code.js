bff7f8b8b4cb86bf55defab0489f5b8f1f85fec1
Bookworm474 <76556656+Bookworm474@users.noreply.github.com>
2021-05-27 20:11:13 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,22 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues(){
+	//clue and hint 1
+	fill("white");
+	textSize(15);
+	text("R E V B A I L A", 100,50);
+	fill("lightblue");
+	text("Hint: Always changing, not constant", 100,70);
+	//clue and hint 2
+	fill("white");
+	text("C U T N I F O N", 700,150);
+	fill("lightblue");
+	text("Hint: Performs a particular task", 700,170);
+	//clue and hint 3
+	fill("white");
+	text("A T A D A S B A", 100,350);
+	fill("lightblue");
+	text("Hint: Stores all information", 100,370);
+}
